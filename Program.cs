8e36de29aea455c2f6b1819be04228c6431a4aa8
hakemjan52 <134385320+hakemjan52@users.noreply.github.com>
2023-05-24 22:38:20 +0430

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Loop
+{
+    class DoWhileLoop
+    {
+        public static void Main(string[] args)
+        {
+            int i = 0;
+
+            do
+            {
+                
+                Console.WriteLine("number is {0}",i);
+                i++;
+            } while (i < 100);
+        }
+    }
+}
